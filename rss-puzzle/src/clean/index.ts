c98b638639aeb70e5cdcbcdf7c556d49bbc269d2
EugeniiSh <evgenii.shmerko@mail.ru>
2025-02-12 13:36:42 +0300

    init: start rss-pz-01

--- rss-puzzle/src/clean/index.ts
+++ rss-puzzle/src/clean/index.ts
@@ -1,5 +1,5 @@
 import { test1 } from './components/test1/test1';
-import { test2 } from './components/test2/test1';
+import {test2} from './components/test2/test1';
 
 // const abc = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 
